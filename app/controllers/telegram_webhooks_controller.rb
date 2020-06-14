c84f682938d639e59af2b0fb13f0cbf6727154d4
Max Melentiev <melentievm@gmail.com>
2020-06-14 21:55:51 +0300

    Reply to unsupported commands

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -6,4 +6,15 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
   def help!(*)
     respond_with :message, text: t('.content')
   end
+
+  def message(message)
+    respond_with :message, text: t('.content', text: message['text'])
+  end
+
+  def action_missing(action, *_args)
+    if action_type == :command
+      respond_with :message,
+        text: t('telegram_webhooks.action_missing.command', command: action_options[:command])
+    end
+  end
 end
