dae1769e115a59ef780e567f2ddc5c870d065f89
Max Melentiev <melentievm@gmail.com>
2020-06-14 21:55:51 +0300

    Reply to unsupported commands

--- spec/requests/telegram_webhooks_spec.rb
+++ spec/requests/telegram_webhooks_spec.rb
@@ -8,4 +8,23 @@ RSpec.describe TelegramWebhooksController, telegram_bot: :rails do
     subject { -> { dispatch_command :help } }
     it { should respond_with_message(/Available cmds/) }
   end
+
+  describe '#message' do
+    subject { -> { dispatch_message text } }
+    let(:text) { 'some plain text' }
+    it { should respond_with_message "You wrote: #{text}" }
+  end
+
+  context 'for unsupported command' do
+    subject { -> { dispatch_command :makeMeGreatBot } }
+    it { should respond_with_message 'Can not perform makeMeGreatBot' }
+  end
+
+  context 'for unsupported feature' do
+    subject { -> { dispatch time_travel: {back_to: :the_future} } }
+    it 'does nothing' do
+      subject.call
+      expect(response).to be_ok
+    end
+  end
 end
